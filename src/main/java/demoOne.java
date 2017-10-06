7ad8ac165f7ccafb29f0e1f283ec04f13442528b
SKY-20170916TCP\Administrator <1006519039@qq.com>
2017-10-06 10:35:52 +0800

    change

--- src/main/java/demoOne.java
+++ src/main/java/demoOne.java
@@ -1,6 +1,12 @@
 public class demoOne {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        //自动装箱
+       Integer ina = 2;
+       Integer inb = 2;
+       System.out.println(ina==inb);
+       Integer in1 = 128;
+       Integer in2 = 128;
+       System.out.println(in1==in2);
     }
 }
