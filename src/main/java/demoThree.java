f8bbdf03b2900703288e853f19b445c1f329efc9
SKY-20170916TCP\Administrator <1006519039@qq.com>
2017-10-06 15:19:40 +0800

    change

--- src/main/java/demoThree.java
+++ src/main/java/demoThree.java
@@ -2,8 +2,32 @@
  * Created by Administrator on 2017/10/6 0006.
  */
 public class demoThree {
+    final int a;
+//    final int b;
+    final String str;
+    final static double d;
+//    final static double d2;
+//    final char ch;
+    {
+        str = "hello";
+    }
+    static
+    {
+        d = 5.6;
+    }
+    public demoThree(){
+        a = 5;
+    }
+    public void change()
+    {
+        //普通方法不能为final变量赋值，只有初始化块，或者是构造器
+        //所以以下三个皆错
+//        d2 = 5.5;
+//        b = 5;
+//        ch = 'a';
+    }
     public static void main(String[] args)
     {
-        System.out.print("111");
+
     }
 }
