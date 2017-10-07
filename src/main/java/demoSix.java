4dbf9c63e2d1f2fe6a95f4403e7c97747bf5e024
SKY-20170916TCP\Administrator <1006519039@qq.com>
2017-10-07 18:07:22 +0800

    Season

--- src/main/java/demoSix.java
+++ src/main/java/demoSix.java
@@ -1,4 +1,14 @@
 /**
  * Created by Administrator on 2017/10/7 0007.
  */
-
+public class demoSix
+{
+    public demoSix(Season season)
+    {
+        System.out.print(season.getName()+"   "+season.getDesc());
+    }
+    public static void main(String[] args)
+    {
+        new demoSix(Season.FALL);
+    }
+}
