5accde400c522798b76feeb35de7e6b72163f785
SKY-20170916TCP\Administrator <1006519039@qq.com>
2017-10-07 14:52:09 +0800

    Outer

---
+++ src/main/java/Outer.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017/10/7 0007.
+ */
+public class Outer {
+    private int outProp = 9;
+    class inner
+    {
+        private int inProp = 5;
+        public void acessOuterProp()
+        {
+            System.out.println(outProp);
+        }
+    }
+    public void acessInnerProp()
+    {
+        System.out.print(new inner().inProp);
+    }
+    public static void main(String[] args)
+    {
+        Outer outer = new Outer();
+        outer.acessInnerProp();
+    }
+}
